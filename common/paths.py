d48a8d4b8f0f40f9d40d2d8c50a6b4fcd2e3195a
Seth A <sba2126@gmail.com>
2026-05-23 18:11:49 -0700

    Start simple MNIST classifier

---
+++ common/paths.py
@@ -0,0 +1,11 @@
+import subprocess
+from pathlib import Path
+
+
+def get_repo_root() -> Path:
+    """Get the repo root directory, expecting this to be a git repository."""
+    return Path(subprocess.check_output(["git", "rev-parse", "--show-toplevel"]).decode().strip())
+
+
+def get_data_dir() -> Path:
+    return get_repo_root() / "data"
