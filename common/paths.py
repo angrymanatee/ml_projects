725d68553d3c9ac979ad45fd6bf32aaaa45a6b3e
Seth A <sba2126@gmail.com>
2026-05-23 18:35:26 -0700

    add pre-commit & cleanup

--- common/paths.py
+++ common/paths.py
@@ -4,7 +4,11 @@ from pathlib import Path
 
 def get_repo_root() -> Path:
     """Get the repo root directory, expecting this to be a git repository."""
-    return Path(subprocess.check_output(["git", "rev-parse", "--show-toplevel"]).decode().strip())
+    return Path(
+        subprocess.check_output(["git", "rev-parse", "--show-toplevel"])
+        .decode()
+        .strip()
+    )
 
 
 def get_data_dir() -> Path:
